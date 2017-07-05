8c26fc330cb8862c5e7858dda3d59ff067b685c7
fullammo <ammoka12@stud.uni-obuda.hu>
2017-07-05 14:09:39 +0200

    JointJS start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,7 @@ import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/
 import { MenuService } from '../fw/services/menu.service';
 import { initialMenuItems } from './app.menu';
 
-import * as $ from 'jquery';
-import * as _ from 'lodash';
-import * as backbone from 'backbone';
-import * as joint from 'jointjs';
+
 import * as jsyaml from 'js-yaml';
 
 @Component({
@@ -39,7 +36,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    let graph = new joint.dia.Graph;
 
     const valamiLeiro = {
       egy: 'egy',
@@ -48,29 +44,5 @@ export class AppComponent implements OnInit {
     }
 
     console.log(jsyaml.dump(valamiLeiro))
-
-    let paper = new joint.dia.Paper({
-      el: $("#paper"),
-      width: 600,
-      height: 200,
-      model: graph,
-      gridSize: 1
-    });
-
-    let rect = new joint.shapes.basic.Rect({
-      position: { x: 100, y: 30 },
-      size: { width: 100, height: 30 },
-      attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
-    });
-
-    let rect2 = rect.clone();
-    // rect2.translate(300);
-
-    var link = new joint.dia.Link({
-      source: { id: rect.id },
-      target: { id: rect2.id }
-    });
-
-    graph.addCells([rect, rect2, link]);
   }
 }
