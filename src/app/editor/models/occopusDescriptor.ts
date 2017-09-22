e831d09407c9f523c9fdab8a2484de1d1cfc432f
fullammo <ammoka12@stud.uni-obuda.hu>
2017-09-22 16:21:38 +0200

    Sendable information to Occopus v0.15

--- src/app/editor/models/occopusDescriptor.ts
+++ src/app/editor/models/occopusDescriptor.ts
@@ -2,7 +2,7 @@ import { NodeDescriptor } from './nodeDescriptor';
 
 // could match the workflow attributes like this.
 export interface OccopusDescriptor {
-    infra_id: number // we used this property in 0.14 version.
+    // infra_id: number // we used this property in 0.14 version.
     user_id: string,
     infra_name: string,
     variables: {
