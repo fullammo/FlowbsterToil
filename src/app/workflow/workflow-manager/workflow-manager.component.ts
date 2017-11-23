7ad13051b8915263e7401c82a986387a6e72d86d
fullammo <ammoka12@stud.uni-obuda.hu>
2017-11-23 15:23:37 +0100

    Multi operations are fully operational

--- src/app/workflow/workflow-manager/workflow-manager.component.ts
+++ src/app/workflow/workflow-manager/workflow-manager.component.ts
@@ -3,6 +3,8 @@ import { WorkflowEntryService } from 'app/workflow/shared/workflow-entry.service
 import { WorkflowEntry } from 'app/workflow/shared/workflowEntry';
 import { MenuItem } from 'primeng/primeng';
 import { Router } from '@angular/router';
+import { OccoService } from 'app/workflow/shared/occo.service';
+import { WorkflowDetailComponent } from 'app/workflow/workflow-detail/workflow-detail.component';
 
 @Component({
   selector: 'toil-workflow-manager',
@@ -17,9 +19,13 @@ export class WorkflowManagerComponent implements OnInit {
 
   constructor(
     private workflowEntrySVC: WorkflowEntryService,
-    private router: Router
+    private router: Router,
+    private occoSVC: OccoService
   ) {}
 
+  /**
+   * We subscibe to any changes made to the Entry FireStore collection.
+   */
   ngOnInit() {
     this.workflowEntrySVC.dataChange.subscribe(entries => {
       this.workflowEntries = entries;
@@ -59,21 +65,65 @@ export class WorkflowManagerComponent implements OnInit {
    * If there is any entry selected it gets saved under a new id into the fireStore
    */
   onMultiCopyClick() {
+    this.IfAnyItemIsSelected(entry => {
+      const peeledEntry = this.workflowEntrySVC.peelEntry(entry);
+      this.workflowEntrySVC.saveEntry(peeledEntry);
+    });
+  }
+
+  /**
+   * If there is any entry selected it gets removed from the fireStore database and also from the selection.
+   */
+  onMultiDeleteClick() {
+    const toBeDeletedFromSelection: WorkflowEntry[] = [];
+
+    this.IfAnyItemIsSelected(entry => {
+      this.workflowEntrySVC.deleteEntry(entry.$key);
+      toBeDeletedFromSelection.push(entry);
+    });
+
+    toBeDeletedFromSelection.forEach(deletedEntry => {
+      this.removeByWorkflowEntryKey(this.selectedWorkflowEntries, deletedEntry);
+    });
+  }
+
+  /**
+   * If there is any entry selected its infrastructure data gets sent via Http to Occopus.
+   */
+  onMultiBuildClick() {
+    this.IfAnyItemIsSelected(entry => {
+      this.occoSVC.buildWorkflow(entry.descriptor, entry.$key);
+    });
+  }
+
+  /**
+   * Checks if there is any workflow selected and gets any job done for each individual item.
+   * @param atomicJob
+   */
+  private IfAnyItemIsSelected(atomicJob: (entry: WorkflowEntry) => any) {
     if (this.selectedWorkflowEntries.length !== 0) {
       this.selectedWorkflowEntries.forEach(entry => {
-        const peeledEntry = this.workflowEntrySVC.peelEntry(entry);
-        this.workflowEntrySVC.saveEntry(peeledEntry);
+        atomicJob(entry);
       });
+    } else {
+      console.log('No Item is selected in the Data Table');
     }
   }
 
   /**
-   * If there is any entry selected it gets removed from the fireStore database.
+   * Removes the Workflow Entry by its key from the given Workflow Entries.
+   * @param array
+   * @param entry
    */
-  onMultiDeleteClick() {
-    if (this.selectedWorkflowEntries.length !== 0) {
-      this.selectedWorkflowEntries.forEach(entry => {
-        this.workflowEntrySVC.deleteEntry(entry.$key);
+  private removeByWorkflowEntryKey(
+    array: WorkflowEntry[],
+    entry: WorkflowEntry
+  ) {
+    if (entry.$key !== undefined) {
+      array.some((item, index) => {
+        return array[index].$key === entry.$key
+          ? !!array.splice(index, 1)
+          : false;
       });
     }
   }
