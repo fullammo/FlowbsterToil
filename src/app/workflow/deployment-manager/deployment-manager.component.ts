8e31c9a144042dffacfae1be1d1de9332d010834
fullammo <ammoka12@stud.uni-obuda.hu>
2018-01-08 11:36:45 +0100

    Workflow Deletion works with Occopus

--- src/app/workflow/deployment-manager/deployment-manager.component.ts
+++ src/app/workflow/deployment-manager/deployment-manager.component.ts
@@ -1,9 +1,11 @@
+import { OccoService } from './../shared/occo.service';
 import { WorkflowEntry } from './../shared/workflowEntry';
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { DeploymentService } from 'app/workflow/shared/deployment.service';
 import { Deployment } from 'app/workflow/shared/deployment';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { ManagerComponent } from 'app/workflow/shared/manager.component';
+import { ConfirmationService } from 'primeng/components/common/confirmationservice';
 
 @Component({
   selector: 'toil-deployment-manager',
@@ -15,10 +17,13 @@ export class DeploymentManagerComponent extends ManagerComponent<
   Deployment,
   WorkflowEntry
 > {
-
   @Output() onWatchClicked = new EventEmitter<string>();
 
-  constructor(private deploymentSVC: DeploymentService) {
+  constructor(
+    private deploymentSVC: DeploymentService,
+    private confirmSVC: ConfirmationService,
+    private occoSVC: OccoService
+  ) {
     super(deploymentSVC);
   }
 
@@ -32,5 +37,18 @@ export class DeploymentManagerComponent extends ManagerComponent<
 
   onMagnifierClicked(entry: Deployment) {
     this.onWatchClicked.emit(entry.graph);
+  }
+
+  confirmDeletion(entry: Deployment) {
+    this.confirmSVC.confirm({
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'fa fa-trash',
+      accept: () => {
+        this.occoSVC.destroyWorkflow(entry.infraid);
+        // only delete the entry when the workflow is actually destroyed
+        this.deploymentSVC.deleteEntry(entry.$key);
+      }
+    });
   }
 }
